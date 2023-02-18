ef8bcaf1ca26d45a10e12a3ea021493f6119251f
MarkMulehi22 <markmulehi@gmail.com>
2023-02-18 04:56:50 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main- program that prints all singl
+ * e digit numbers of base 10 starting from 0
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
