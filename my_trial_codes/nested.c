ffa5cecf7467f0d31f42cd5858191eacf7a2a7eb
MarkMulehi22 <markmulehi@gmail.com>
2023-02-21 07:45:52 -0800

    My first trial codes

---
+++ my_trial_codes/nested.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -Program to test while loop to make a table
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	int i = 0, ji;
+
+	while (i < 5)
+	{
+		j = 0;
+		while (j<3)
+		{
+			printf("2");
+			j++;
+		}
+		printf("n");
+		i++
+	}
+	return (0);
+}
