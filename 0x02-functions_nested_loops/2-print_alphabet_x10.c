ed5c09cb599a6baae8d2d1fa0ed19475f051501a
MarkMulehi22 <markmulehi@gmail.com>
2023-02-21 19:06:00 -0800

    Task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10-> a function that prints 10 times the alphabet
+ * Return: 0 (Suceess)
+ */
+
+void print_alphabet_x10(void)
+{
+	int k;
+	int j;
+
+	for (k = 0; k < 10; k++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
