f10abad30d0766b0bbd4db7a7d77e1bb6c65f759
MarkMulehi22 <markmulehi@gmail.com>
2023-02-23 03:39:15 -0800

    Task 7

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,13 +7,13 @@
 
 void print_most_numbers(void)
 {
-	int x;
+	int c;
 
-	for (x = 0; x <= 9; x++)
+	for (c = 0; c <= 9; c++)
 	{
-		if (x != 2 && x != 4)
+		if (c != 2 && c != 4)
 		{
-			_putchar(x + '0')
+			_putchar(c + '0')
 		}
 	}
 	_putchar('\n');
