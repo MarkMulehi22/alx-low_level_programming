a529df41bf8cafd5ac557236ab226d376cb88a0f
MarkMulehi22 <markmulehi@gmail.com>
2023-02-23 03:17:04 -0800

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers- function that prints the numbers
+ * Return: 0 (Success)
+ */
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		if (x != 2 && x != 4)
+		{
+			_putchar(x + '0')
+		}
+	}
+	_putchar('\n');
+}
